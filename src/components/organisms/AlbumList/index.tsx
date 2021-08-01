aa6427d878bb2ba7781cea2c96ac59e0fe898e80
Andres <acastri04@gmail.com>
2021-08-01 14:48:14 -0600

    First upload

--- src/components/organisms/AlbumList/index.tsx
+++ src/components/organisms/AlbumList/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import { View, ActivityIndicator, FlatList } from "react-native"
+import { View, ActivityIndicator, FlatList, Text, StyleSheet } from "react-native"
 import { useDispatch, useSelector } from "react-redux"
 import IAlbum from "../../../models/IAlbum"
 import { IState } from "../../../models/IState"
@@ -17,21 +17,34 @@ const AlbumList: React.FC = () => {
     return (
         <View>
             {albums.length > 0 ? (
-                <FlatList
-                    data={albums}
-                    renderItem={({ item, index }) => (
-                        <AlbumListItem
-                            key={item.id}
-                            album={item}
-                            index={index}
-                        />
-                    )}
-                />
+                <>
+                    <Text style={styles.center}>Lista de albumes</Text>
+                    <FlatList
+                        data={albums}
+                        renderItem={({ item, index }) => (
+                            <AlbumListItem
+                                key={item.id}
+                                album={item}
+                                index={index}
+                            />
+                        )}
+                    />
+                </>
             ) : (
                 <ActivityIndicator color="#000" />
             )}
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    center: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+    }
+});
 
 export default AlbumList
